ff2b1c7728313019d3c010071bb787bbd7397e94
Pablito De Cecco <pablomdececcorios@gmail.com>
2019-04-03 18:48:07 -0300

    ionicv4 with firestore

---
+++ IonicTestApp/src/environments/environment.ts
@@ -0,0 +1,24 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAz37i4MiXhLNbNgbZH1jER8JH1SDYOBME",
+    authDomain: "ufm-app.firebaseapp.com",
+    databaseURL: "https://ufm-app.firebaseio.com",
+    projectId: "ufm-app",
+    storageBucket: "ufm-app.appspot.com",
+    messagingSenderId: "740572839010"    
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
